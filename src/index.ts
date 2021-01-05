890112d60de908599383004d6d24f6f64a67af16
Cyril Wanner <info@cyr.li>
2021-01-05 23:16:27 +0000

    Bundle all classes in one index file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Config } from './Config';
+export { default as Points } from './types/Points';
+export { default as RawGroup } from './types/RawGroup';
+export { default as RawTest } from './types/RawTest';
+export { default as Serve } from './types/Serve';
