6cca34cb14b43adc50f1dde47ceed172e5d9f249
Cyril Wanner <info@cyr.li>
2021-01-05 23:38:00 +0000

    Use default export for config class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { default as Config } from './Config';
+export { default } from './Config';
 export { default as Points } from './types/Points';
 export { default as RawGroup } from './types/RawGroup';
 export { default as RawTest } from './types/RawTest';
